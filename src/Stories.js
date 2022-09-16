067fa38ab02fcfb07294407267a3512f0df757c7
Vitor Torii <vitortorii@gmail.com>
2022-09-16 00:41:12 -0300

    feat: add components/function map/props

---
+++ src/Stories.js
@@ -0,0 +1,35 @@
+function Dados(props){
+return(        
+    <div class="story">
+        <div class="imagem">
+            <img src={props.imagem} />
+        </div>
+        <div class="usuario">
+            {props.usuario}
+        </div>
+    </div>
+    );
+}
+
+export default function Stories(){
+    const dados = [
+        {imagem:"./img/9gag.svg", usuario:"9gag"},
+        {imagem:"./img/meowed.svg", usuario:"meowed"},
+        {imagem:"./img/barked.svg", usuario:"barked"},
+        {imagem:"./img/nathanwpylestrangeplanet.svg", usuario:"nathanwpylestrangeplanet"},
+        {imagem:"./img/wawawicomics.svg", usuario:"wawawicomics"},
+        {imagem:"./img/respondeai.svg", usuario:"respondeai"},
+        {imagem:"./img/filomoderna.svg", usuario:"filomoderna"},
+        {imagem:"./img/memeriagourmet.svg", usuario:"memeriagourmet"}
+    ];
+
+    return(    
+    <div class="stories">
+        {dados.map((d) => <Dados imagem={d.imagem} usuario={d.usuario}/>)};  
+
+        <div class="setinha">
+            <ion-icon name="chevron-forward-circle"></ion-icon>
+        </div>
+    </div>
+    );
+}
